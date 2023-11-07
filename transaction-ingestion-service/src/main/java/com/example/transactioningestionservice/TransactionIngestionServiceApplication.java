d7be5e52ebbb0667f7db6c611eb8470fcd3355df
dmitrynovik <cryinstone@gmail.com>
2023-11-07 13:43:24 +1100

    chore: restructuring for multiple projects

--- transaction-ingestion-service/src/main/java/com/example/transactioningestionservice/TransactionIngestionServiceApplication.java
+++ transaction-ingestion-service/src/main/java/com/example/transactioningestionservice/TransactionIngestionServiceApplication.java
@@ -3,10 +3,14 @@ package com.example.transactioningestionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import common.Library;
+
 @SpringBootApplication
 public class TransactionIngestionServiceApplication {
 
 	public static void main(String[] args) {
+		Library lib = new Library();
+		//lib.
 		SpringApplication.run(TransactionIngestionServiceApplication.class, args);
 	}
 
