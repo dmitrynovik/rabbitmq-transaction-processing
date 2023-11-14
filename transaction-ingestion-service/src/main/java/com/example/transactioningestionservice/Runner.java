de2cbeb478e01a7025db5887670859ebc4b5642b
Udo Kohlmeyer <udo@vmare.com>
2023-11-14 14:27:36 +1100

    Did something and now it works

--- transaction-ingestion-service/src/main/java/com/example/transactioningestionservice/Runner.java
+++ transaction-ingestion-service/src/main/java/com/example/transactioningestionservice/Runner.java
@@ -1,33 +1,35 @@
 package com.example.transactioningestionservice;
 
-import java.io.IOException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class Runner implements CommandLineRunner {
 
-    private RabbitTemplate rabbitTemplate;
+  private RabbitTemplate rabbitTemplate;
 
-    public Runner(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
+  public Runner(RabbitTemplate rabbitTemplate) {
+    this.rabbitTemplate = rabbitTemplate;
+  }
+
+  @Override
+  public void run(String... args) {
+    try {
+      System.out.println("RABBIT QUEUE: " + this.rabbitTemplate.getDefaultReceiveQueue());
+
+
+      int customers = new CustomerService().getAll().size();
+      System.out.println("CUSTOMERS: " + customers);
+
+      int tx = new TransactionService().getAll().size();
+      System.out.println("TX: " + tx);
+//
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    @Override
-    public void run(String... args) {
-        try {
-            System.out.println("RABBIT QUEUE: " + this.rabbitTemplate.getDefaultReceiveQueue());
-
-			int customers = new CustomerService().getAll().size();
-			System.out.println("CUSTOMERS: " + customers);
-
-			int tx = new TransactionService().getAll().size();
-			System.out.println("TX: " + tx);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
-    
 }
