dbd63a0c6525c4bb9937e3acc3919820ce85ed24
dmitrynovik <dmitry.novik@gmail.com>
2023-11-15 16:12:38 +1100

    feat: dynamic number of queues

--- transaction-ingestion-service/src/main/java/com/example/transactioningestionservice/Runner.java
+++ transaction-ingestion-service/src/main/java/com/example/transactioningestionservice/Runner.java
@@ -1,46 +1,91 @@
 package com.example.transactioningestionservice;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import com.rabbitmq.client.Channel;
 
 import common.data.Transaction;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
 
 @Component
 public class Runner implements CommandLineRunner {
 
+  static final String exchangeName = "txExchange";
+  static final String queuePrefix = "txQueue_";  
+  static final Map<String, Object> quorumQueueArgs = Map.of("x-queue-type", "quorum");
   private RabbitTemplate rabbitTemplate;
+  private int queues = 4;
 
-  public Runner(RabbitTemplate rabbitTemplate) {
+  public Runner(RabbitTemplate rabbitTemplate) throws IOException, TimeoutException {
     this.rabbitTemplate = rabbitTemplate;
+    createRabbitMQTopology();
+  }
+
+  private void createRabbitMQTopology() throws IOException, TimeoutException {
+    Channel ch = rabbitTemplate.getConnectionFactory()
+      .createConnection()
+      .createChannel(false);
+
+    try {
+      // TODO: properties
+      queues = 4;//Integer.parseInt( System.getProperty("rabbitmq.queues") );
+      
+      List<String> queueNames = new ArrayList<>(queues);
+      for (int i = 1; i <= queues; ++i)
+        queueNames.add(queuePrefix + i);
+
+      // Declare RabbitMQ queues of type quorum:
+      for (String q : queueNames) {
+        System.out.println("DECLARING QUEUE " + q);
+        ch.queueDeclare(q, true, false, false, quorumQueueArgs);
+        ch.queuePurge(q);
+      }
+
+      // Declare RabbitMQ exchange of type consistent hash:
+      ch.exchangeDeclare(exchangeName, "x-consistent-hash", true, false, null);
+
+      int i = 0;
+      for (String q : queueNames) {
+        ch.queueBind(q, exchangeName, String.valueOf(++i));
+      }
+    } finally {
+      ch.close();
+    }
   }
 
   @Override
   public void run(String... args) throws InterruptedException {
     try {
-      System.out.println("RABBIT XCHANGE: " + this.rabbitTemplate.getExchange());
-
-      //   int customers = new CustomerService().getAll().size();
-      //   System.out.println("CUSTOMERS: " + customers);
-
       List<Transaction> tx = new TransactionService().getAll();
+
       for (int i = 0; i < tx.size(); ++i) {
-        System.out.println("Sending customer: " + tx.get(i).seqNumber1);
-        this.rabbitTemplate.convertAndSend(TransactionIngestionServiceApplication.exchangeName, 
+        System.out.println("Sending transaction: " + tx.get(i).seqNumber1);
+        this.rabbitTemplate.convertAndSend(exchangeName, 
             String.valueOf((i % 4) + 1),
             tx.get(i));
 
         Thread.sleep(1000);
       }
-
-
-
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
+  private Queue createQuorumQueue(String queueName) {
+		return new Queue(queueName, 
+      true, 
+      false, 
+      false, 
+      quorumQueueArgs);
+	}
 }
--- transaction-ingestion-service/src/main/java/com/example/transactioningestionservice/TransactionIngestionServiceApplication.java
+++ transaction-ingestion-service/src/main/java/com/example/transactioningestionservice/TransactionIngestionServiceApplication.java
@@ -1,126 +1,86 @@
 package com.example.transactioningestionservice;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.CustomExchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+// import java.util.Map;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.amqp.core.CustomExchange;
+// import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+// import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+// import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 
 @SpringBootApplication
 public class TransactionIngestionServiceApplication {
 
-	static final String exchangeName = "txExchange";
-	static final String queuePrefix = "txQueue_";
-	static final Map<String, Object> queueArgs = new HashMap<>();
+	//static final Map<String, Object> queueArgs = Map.of("x-queue-type", "quorum");
 
-	public TransactionIngestionServiceApplication() {
-		queueArgs.put("x-queue-type", "quorum");
-	}
+	// @Bean
+	// CustomExchange exchange() {
+	// 	System.out.println("CREATE EXCHANGE " + exchangeName);
+	// 	return new CustomExchange(exchangeName, "x-consistent-hash");
+	// }
 
-	@Bean
-	CustomExchange exchange() {
-		System.out.println("CREATE EXCHANGES...");
-		return new CustomExchange(exchangeName, "x-consistent-hash");
-	}
 
-	@Bean
-	Queue[] queues() {
-		System.out.println("CREATE QUEUES...");
-		Queue[] queues = new Queue[4];
-		Map<String, Object> args = new HashMap<>();
-		args.put("x-queue-type", "quorum");
-		for (int i = 0; i < queues.length; ++i) {
-			queues[i] = new Queue(queuePrefix + i, true, false, true, args);
-		}
-		return queues;
-	}
+	// @Bean Queue queue1() {
+	// 	return new Queue(queuePrefix + 1, true, false, false, queueArgs);
+	// }
 
-	@Bean Queue queue1() {
-		return new Queue(queuePrefix + 1, true, false, false, queueArgs);
-	}
+	// @Bean Binding binding1(Queue queue1, CustomExchange exchange) {
+	// 	return BindingBuilder.bind(queue1)
+	// 			.to(exchange)
+	// 			.with("1")
+	// 			.noargs();
+	// }
 
-	@Bean Binding binding1(Queue queue1, CustomExchange exchange) {
-		return BindingBuilder.bind(queue1)
-				.to(exchange)
-				.with("1")
-				.noargs();
-	}
+	// @Bean Binding binding2(Queue queue2, CustomExchange exchange) {
+	// 	return BindingBuilder.bind(queue2)
+	// 			.to(exchange)
+	// 			.with("2")
+	// 			.noargs();
+	// }
 
-	@Bean Binding binding2(Queue queue2, CustomExchange exchange) {
-		return BindingBuilder.bind(queue2)
-				.to(exchange)
-				.with("2")
-				.noargs();
-	}
+	// @Bean Binding binding3(Queue queue3, CustomExchange exchange) {
+	// 	return BindingBuilder.bind(queue3)
+	// 			.to(exchange)
+	// 			.with("3")
+	// 			.noargs();
+	// }
 
-	@Bean Binding binding3(Queue queue3, CustomExchange exchange) {
-		return BindingBuilder.bind(queue3)
-				.to(exchange)
-				.with("3")
-				.noargs();
-	}
+	// @Bean Binding binding4(Queue queue4, CustomExchange exchange) {
+	// 	return BindingBuilder.bind(queue4)
+	// 			.to(exchange)
+	// 			.with("4")
+	// 			.noargs();
+	// }
 
-	@Bean Binding binding4(Queue queue4, CustomExchange exchange) {
-		return BindingBuilder.bind(queue4)
-				.to(exchange)
-				.with("4")
-				.noargs();
-	}
+	// @Bean Queue queue2() {
+	// 	return new Queue(queuePrefix + 2, true, false, false, queueArgs);
+	// }
 
-	@Bean Queue queue2() {
-		return new Queue(queuePrefix + 2, true, false, false, queueArgs);
-	}
+	// @Bean Queue queue3() {
+	// 	return new Queue(queuePrefix + 3, true, false, false, queueArgs);
+	// }
 
-	@Bean Queue queue3() {
-		return new Queue(queuePrefix + 3, true, false, false, queueArgs);
-	}
+	// @Bean Queue queue4() {
+	// 	return new Queue(queuePrefix + 4, true, false, false, queueArgs);
+	// }
 
-	@Bean Queue queue4() {
-		return new Queue(queuePrefix + 4, true, false, false, queueArgs);
-	}
-
-	@Bean
-	List<Binding> binding(Queue[] queues, CustomExchange exchange) {
-		System.out.println("CREATE BINDINGS...");
-		List<Binding> bindings = new ArrayList<>();
-		for (int i = 0; i < queues.length; ++i) {
-			Binding binding = BindingBuilder.bind(queues[i])
-				.to(exchange)
-				.with("")
-				.noargs();
-
-			bindings.add(binding);
-		}
-		return bindings;
-	}
-	
-	@Bean
-	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-			MessageListenerAdapter listenerAdapter) {
+	// @Bean
+	// SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+	// 		MessageListenerAdapter listenerAdapter) {
 		
-		System.out.println("CREATING CONTAINER");
-		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.setQueueNames("txQueue_1", "txQueue_2", "txQueue_3", "txQueue_4");
-		container.setMessageListener(listenerAdapter);
-		return container;
-	}
+	// 	System.out.println("CREATING CONTAINER");
+	// 	SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+	// 	container.setConnectionFactory(connectionFactory);
+	// 	container.setQueueNames("txQueue_1", "txQueue_2", "txQueue_3", "txQueue_4");
+	// 	container.setMessageListener(listenerAdapter);
+	// 	return container;
+	// }
 
-	@Bean
-	MessageListenerAdapter listenerAdapter(Receiver receiver) {
-		return new MessageListenerAdapter(receiver, "receiveMessage");
-	}
+	// @Bean
+	// MessageListenerAdapter listenerAdapter(Receiver receiver) {
+	// 	return new MessageListenerAdapter(receiver, "receiveMessage");
+	// }
 
 	public static void main(String[] args) {
 		System.setProperty("spring.amqp.deserialization.trust.all","true");
