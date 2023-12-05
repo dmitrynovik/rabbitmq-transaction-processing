7770352d82ebb89687279082cefcc9513a01c402
dmitrynovik <dmitry.novik@gmail.com>
2023-12-05 19:36:40 +1100

    chore: minor cleanup

--- transaction-ingestion-service/src/main/java/com/example/transactioningestionservice/PublisherService.java
+++ transaction-ingestion-service/src/main/java/com/example/transactioningestionservice/PublisherService.java
@@ -13,7 +13,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.availability.AvailabilityChangeEvent;
 import org.springframework.boot.availability.LivenessState;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
