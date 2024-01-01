6b179476c3e3fcc8aef71bfcaa327ebd4999e105
dmitrynovik <dmitry.novik@gmail.com>
2024-01-01 17:24:15 +1100

    test: transaction-ingestion-service

--- transaction-ingestion-service/src/main/java/com/example/transactioningestionservice/ApplicationService.java
+++ transaction-ingestion-service/src/main/java/com/example/transactioningestionservice/ApplicationService.java
@@ -1,11 +1,20 @@
 package com.example.transactioningestionservice;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApplicationService {
 
-    public ApplicationService(PublisherService publisherService) throws InterruptedException {
+    private PublisherService publisherService;
+
+    public ApplicationService(PublisherService publisherService) {
+        this.publisherService = publisherService;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onReady() throws InterruptedException {
         publisherService.run();
         publisherService.startPubishing();
     }
--- transaction-ingestion-service/src/test/java/com/example/transactioningestionservice/TransactionIngestionServiceApplicationTests.java
+++ transaction-ingestion-service/src/test/java/com/example/transactioningestionservice/TransactionIngestionServiceApplicationTests.java
@@ -2,10 +2,14 @@ package com.example.transactioningestionservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class TransactionIngestionServiceApplicationTests {
-
+    
+    @MockBean
+    private ApplicationService applicationService;
+    
 	@Test
 	void contextLoads() {
 	}
